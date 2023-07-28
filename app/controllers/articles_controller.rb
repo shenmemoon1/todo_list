366b05b42191fa6048e6e304f01bf1c01bbe4a85
billlims <davidlinxf@gmail.com>
2023-07-28 18:04:05 +1000

    #update-edit-form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all
+    # @articles = Article.all
+    @articles = Article.all.order(created_at: :desc)
   end
 
   def show
@@ -14,10 +15,26 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     if @article.save
+      flash[:notice] = 'Article created successfully.'
+      redirect_to @article
+    else
+      flash[:alert] = 'Failed to create article.'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.new(params[:id])
+    if @article.update(article_params)
       redirect_to @article
     else
       render :new, status: :unprocessable_entity
     end
+
   end
 
   private
